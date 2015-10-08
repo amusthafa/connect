3f675ba3d958a45432d7e0bcbc40e5127668e9ed
Vardhini M <vardhini.m25@gmail.com>
2015-10-08 05:29:20 -0700

    Embedding profile,address in user model and added comments in Signup

--- server/create/signUpServer.js
+++ server/create/signUpServer.js
@@ -7,31 +7,62 @@ Meteor.methods({signUp: function (userProfile) {
     password = userProfile.password;
     Accounts.createUser({
       email:email,
-      password: password
+      password: password,
+      profile : {
+        "firstName": userProfile.firstName,
+        "lastName": userProfile.lastName,
+        "birthday": userProfile.dob,
+        "gender": userProfile.gender,
+        "organization_flag": userProfile.orgFlag,
+        "organization": userProfile.organisationName,
+        "occupation": userProfile.occupation,
+        "phone": userProfile.mobileNo,
+        "share_phone": userProfile.shareNo,
+        "status": "Active",
+        "comments": userProfile.comments,
+        "differently_abled": userProfile.diffAbled,
+        "address": {line1: userProfile.addr1,
+        line2: userProfile.addr2,
+        city: userProfile.city,
+        state: userProfile.state,
+        country: userProfile.country,
+        pinCode: userProfile.pincode,
+        primary: "Yes"},
+        "app_role": userProfile.role
+      },
+      "roles": "Admin"
     });
-    address = new Mongo.Collection("Address");
-    tempAddr = address.insert({
-      line1: userProfile.addr1,
-      line2: userProfile.addr2,
-      city: userProfile.city,
-      state: userProfile.state,
-      country: userProfile.country,
-      pinCode: userProfile.pincode,
-      primary: "Yes"
-    });
-    Profile = new Mongo.Collection("UserProfile");
-    Profile.insert({
-    firstName: userProfile.firstName,
-    lastName: userProfile.lastName,
-    birthday: userProfile.dob,
-    gender: userProfile.gender,
-    organization_flag: userProfile.orgFlag,
-    organization: userProfile.organisationName,
-    occupation: userProfile.occupation,
-    phone: userProfile.mobileNo,
-    share_phone: userProfile.shareNo,
-    status: "Active",
-    address: tempAddr,
-    app_role: "User"
-    });
+    // address = new Mongo.Collection("Address");
+    // tempAddr = address.insert({
+    //   line1: userProfile.addr1,
+    //   line2: userProfile.addr2,
+    //   city: userProfile.city,
+    //   state: userProfile.state,
+    //   country: userProfile.country,
+    //   pinCode: userProfile.pincode,
+    //   primary: "Yes"
+    // });
+    // Profile = new Mongo.Collection("UserProfile");
+    // Profile.insert({
+    // "firstName": userProfile.firstName,
+    // "lastName": userProfile.lastName,
+    // "birthday": userProfile.dob,
+    // "gender": userProfile.gender,
+    // "organization_flag": userProfile.orgFlag,
+    // "organization": userProfile.organisationName,
+    // "occupation": userProfile.occupation,
+    // "phone": userProfile.mobileNo,
+    // "share_phone": userProfile.shareNo,
+    // "status": "Active",
+    // "comments": userProfile.comments,
+    // "differently_abled": userProfile.diffAbled,
+    // "address": {line1: userProfile.addr1,
+    // line2: userProfile.addr2,
+    // city: userProfile.city,
+    // state: userProfile.state,
+    // country: userProfile.country,
+    // pinCode: userProfile.pincode,
+    // primary: "Yes"},
+    // "app_role": userProfile.role
+    // });
 }});
