bee46bdf1a0d66a910442cb9f21a402d4541e07c
amusthafa <amusthafa@paypal.com>
2015-10-16 00:16:39 +0530

    Scheduled tasks initial commit, Match, connect changes

---
+++ server/create/scheduledTasks.js
@@ -0,0 +1,203 @@
+/**
+ * Created by amusthafa on 10/14/2015.
+ */
+SyncedCron.add({
+    name: 'Scheduled task for Connect',
+    schedule: function(parser) {
+        // parser is a later.parse object
+        return parser.text('every 1 minutes');
+    },
+    job: function() {
+        console.log("=================Scheduled job start=================");
+
+         //Scheduled Task 1
+        //if request submitted, match available,
+        // send notification to seeker, if noti not already sent or read
+
+        var requestList= Request.find({status : "Submitted" ,requiredBy : {$gt:new Date()}}).fetch();
+
+        //Chk if Connect Notification already available, type= submitted userid = seeker
+        for (var i in requestList) {
+
+            var request = requestList[i];
+
+            var notification = Notifications.findOne({userId : request.requestorId, status : 'Unread', type : "Submitted" });
+            //if there is no existing notification, run match to check if match exists
+           console.log('notification ' + notification);
+            if (!notification){
+                var req= {_id :request._id};
+                Meteor.call('matchRequestVolunteer',req, function(err, result) {
+                    console.log("on rendered result:", JSON.stringify(result));
+
+                    if (result.volunteerList.length >0){
+
+                        //Entry in notification table
+
+                        var notificationData = {
+                            requestorId: request.requestorId,
+                          //  connectId: ,
+                            requestId: request._id,
+                            volunteerAidId: request.aidId,
+                            status: 'Unread',
+                            userId: request.requestorId,
+                            type: 'Submitted',
+                            description: 'Matches available for ' + request.request_name
+                        };
+                        Notifications.insert(notificationData, function (error, result) {
+                            console.log("notification id - " + result);
+                            if (error) {
+                                console.log("Errors !!" + error + "  Result - " + result);
+                                //TO-DO: error message()
+                                // throw new Meteor.Error("insert-failed", error.message);    });
+                                throw new Meteor.Error("insert-failed", error);
+                            }});
+                    }
+
+                });
+
+            }
+        }
+
+        //Scheduled Task 2
+        //If Request Date is reached for Request,
+        // update connect status, Accepted -> Pending Completion
+        // Send noti to seeker for marking complete
+        // If Request is still in Submitted status, mark Closed
+
+        var dateRequestList= Request.find({requiredBy : {$lte:new Date()}, status : 'InProgress' }).fetch();
+
+        for (var i in dateRequestList) {
+
+            var request = dateRequestList[i];
+            var connectUpdate = {status : 'Pending Completion'}
+
+            Connect.update({_id : request._id,status :'Accepted'},
+                {$set:connectUpdate}, function (error, result) {
+
+                console.log("Connect update " + JSON.stringify(result));
+                if (error) {
+                    console.log("Errors !!" + error + "  Result - " + result);
+                    //TO-DO: error message()
+                    // throw new Meteor.Error("insert-failed", error.message);
+                    throw new Meteor.Error("update-failed", error);
+                }
+            });
+            //send notification to seeker for marking complete
+            //Entry in notification table
+
+            var notificationData = {
+                requestorId: request.requestorId,
+                //  connectId: ,
+                requestId: request._id,
+                volunteerAidId: request.aidId,//???
+                status: 'Unread',
+                userId: request.requestorId,
+                type: 'PendingCompletion',
+                description: 'Was the request ' + request.request_name + ' successful. Please update us about how it went.'
+            };
+            Notifications.insert(notificationData, function (error, result) {
+                console.log("notification id - " + result);
+                if (error) {
+                    console.log("Errors !!" + error + "  Result - " + result);
+                    //TO-DO: error message()
+                    // throw new Meteor.Error("insert-failed", error.message);    });
+                    throw new Meteor.Error("insert-failed", error);
+                }});
+
+
+        }
+
+        // If Request is still in Submitted status, mark Closed
+       var requestUpdate= {status : "Closed"};
+        Request.update({status : "Submitted" ,requiredBy : {$eq:new Date()}},
+            {$set:requestUpdate}, function (error, result) {
+
+                console.log("Request update " + JSON.stringify(result));
+                if (error) {
+                    console.log("Errors !!" + error + " Updated Requests as they are Submitted but reached requiredby date - " + result);
+                    //TO-DO: error message()
+                    // throw new Meteor.Error("insert-failed", error.message);
+                    throw new Meteor.Error("update-failed", error);
+                }
+            });
+
+        //Scheduled Task 3
+        //If request in Pending completion status, no noti or noti is not Unread then create new noti
+
+        //find list of requests with status pending completion
+
+        var pcConnectList= Connect.find({status : 'PendingCompletion'}).fetch();
+
+        for (var i in pcConnectList) {
+        var connect = pcConnectList[i];
+            //searching for seeker noti
+            var notification = Notifications.findOne({ type : 'PendingCompletion',
+                status: 'Unread', requestId: connect.requestId});
+
+            if (!notification){
+                //Entry in notification table
+
+                var notificationData = {
+                    requestorId: connect.requestorId,
+                    connectId:connect._id ,
+                    requestId: connect.requestId,
+                    volunteerAidId: connect.volunteerAidId,
+                    status: 'Unread',
+                    userId: connect.requestorId,
+                    type: 'PendingCompletion',
+                    description: 'Was the request ' + request.request_name + ' successful. Please update us about how it went.'
+                };
+                Notifications.insert(notificationData, function (error, result) {
+                    console.log("notification id - " + result);
+                    if (error) {
+                        console.log("Errors !!" + error + "  Result - " + result);
+                        //TO-DO: error message()
+                        // throw new Meteor.Error("insert-failed", error.message);    });
+                        throw new Meteor.Error("insert-failed", error);
+                    }});
+            }
+
+        }
+
+        //Scheduled Task 4
+        //If connect completed by seeker and marked so, rating not provided by volunteer
+        // then send noti to volunteer to provide rating, check if noti already exists
+
+        var cConnectList= Connect.find({status : 'Completed', requestorRating :0 }).fetch();
+
+        for (var i in cConnectList) {
+            var connect = cConnectList[i];
+
+            var notification = Notifications.findOne({ type : 'Completed',
+                status: 'Unread', requestId: connect.requestId});
+
+            if (!notification){
+                //Entry in notification table
+
+                var notificationData = {
+                    requestorId: connect.requestorId,
+                      connectId:connect._id ,
+                    requestId: connect.requestId,
+                    volunteerAidId: connect.volunteerAidId,
+                    status: 'Unread',
+                    userId: connect.volunteerId,
+                    type: 'Completed',
+                    description: 'How was your experience for ' + request.request_name + '. Can you rate the Seeker for us.'
+                };
+                Notifications.insert(notificationData, function (error, result) {
+                    console.log("notification id - " + result);
+                    if (error) {
+                        console.log("Errors !!" + error + "  Result - " + result);
+                        //TO-DO: error message()
+                        // throw new Meteor.Error("insert-failed", error.message);    });
+                        throw new Meteor.Error("insert-failed", error);
+                    }});
+            }
+
+        }
+
+
+        console.log("=================Scheduled job end=================");
+        return true;
+    }
+});
