05ffb2aed593d1ea0c0d07c020c67a905cfa44f1
Tamilppavai Ramamoorthy <tramamoorthy@paypal.com>
2015-10-17 13:53:29 +0530

    Search - view profile

--- client/views/Search/SearchId.js
+++ client/views/Search/SearchId.js
@@ -1,69 +1,123 @@
+//Session.setDefault("res","Self");
 Template.SearchId.helpers({
-    'Search': function () {
-        return "Search Profile";
+      'Search': function () {
+        check();
+      return (Session.get('res'));
     }
 });
 
 //UserprofileDummy = new Meteor.Collection('userprofiles');
 
-Meteor.users.initEasySearch('profile.firstName', {
-    'limit' : 20,
-    'use' : 'mongo-db'
-});
+
 Template.SearchId.events({
     //'click .search': function (event, next) {
       //  name = document.getElementById('name').value;
-            'submit form': function (event) {
+             'submit.search': function (event) {
         event.preventDefault();
         EnteredName = event.target.Ename.value;
         console.log('clicked Search ' + EnteredName );
         Meteor.call("SearchUser",EnteredName, function(error, result) {
                //console.log ("the result is:" User_result[0].name)
-        console.log("Client : error" + error + JSON.stringify(result));
-        if (result == 0) {
-          console.log("No Result Found.")
-        } else {
-          var userdetail ='<table border=1>';
-          for(var key in result)
-          {
-            userdetail += '<tr><td>';
-            for(var field in result[key]){
-              if (typeof result[key][field] == 'object') {
-                if (field.match("profile")) {
-                  var temp = {};
-                  temp = result[key].profile;
-                  for (var k in temp) {
-                    console.log("My test: " + k);
-                    if (typeof temp[k] == 'object') {
-                      if (k.match("address")) {
-                        var temp2 = {};
-                        temp2 = temp[k];
-                        userdetail +=  k + ":";
-                        for (var l in temp2) {
-                          if (l.match("primary"))
-                            continue;
-                          userdetail +=  temp[k][l] +'</br>';
+              // console.log("Client : error" + error + JSON.stringify(result));
+               if (result == 0) {
+                 console.log("No Result Found.");
+                 alert("No Result Found !")
+               }
+               else {
+                 Session.set('res',result);
+
+                 var userview = '<table border=2 >';
+                 for(var key in result)
+                 {
+                   userview += '<tr>';
+                   for(var field in result[key]){
+                     if (typeof result[key][field] == 'object') {
+                       if (field.match("profile")) {
+                         var temp = {};
+                         temp = result[key].profile;
+                         for (var k in temp) {
+                           console.log("My test: " + k);
+                           if (k.match("firstName") || k.match("lastName") )
+                           {
+                                  userview += '<td>'+'<b>'+ k.toUpperCase() +'</b>'+ '</td>' ;
+                                 userview += '<td>'+temp[k] +'</br>'+'</td>';
+                                 userview += '</td></tr>';
+                               //(k.match("sharePhone") and temp[k]='yes')
+                             }
+                                                    }
                         }
                       }
-                    } else {
-                      userdetail +=  k + ":";
-                      userdetail +=  temp[k] +'</br>';
                     }
                   }
-                } else {
-                  continue;
-                }
-              } else {
-                continue;
-              }
-            }
-            userdetail += '</td></tr>';
-               //userdetail += "Name  :"+result[key].firstName + result[key].lastName +"<br>"+ "gender:" +result[key].gender+"<br>";
-          }
-          userdetail += '</table>';
-          document.getElementById("SearchResult").innerHTML = userdetail;
-        }
+                    userview += '<td>'+ '<button type="button" class="view" value= "view" >'+'view'+'</button>'+' </td>';
+                  //userview += '<td>' + '<a href="Router.go('/');">'+'view'+ '</a></td>';
+                userview += '</table>';
+           }
+                 document.getElementById("SearchResult").innerHTML = userview;
 
-        });
-      }
-});
+              });
+            },
+
+            'click.view': function () {
+       event.preventDefault();
+       userprofileDetail={}
+        userprofileDetail=Session.get('res');
+           console.log('clicked view '+ + JSON.stringify(userprofileDetail));
+      // Meteor.call("SearchUser",EnteredName, function(error, result) {
+
+            console.log("value is:"+Session.get('res'));
+          }
+
+       });
+
+
+
+
+
+        //          var userdetail ='<table border=2 >';
+        //          for(var key in result)
+        //          {
+        //            userdetail += '<tr>';
+        //            for(var field in result[key]){
+        //              if (typeof result[key][field] == 'object') {
+        //                if (field.match("profile")) {
+        //                  var temp = {};
+        //                  temp = result[key].profile;
+        //                  for (var k in temp) {
+        //                    console.log("My test: " + k);
+        //                    if (k.match("firstName") || k.match("lastName") || k.match("gender") || k.match("organisation") || k.match("occupation") )
+        //                    {
+        //                           userdetail += '<td>'+'<b>'+ k.toUpperCase() +'</b>'+ '</td>' ;
+        //                          userdetail += '<td>'+temp[k] +'</br>'+'</td>';
+        //                          userdetail += '</td></tr>';
+        //                        //(k.match("sharePhone") and temp[k]='yes')
+        //                      }
+        //
+        //                    if (typeof temp[k] == 'object') {
+        //                      if (k.match("address")) {
+        //                        var temp2 = {};
+        //                        temp2 = temp[k];
+        //                        userdetail += '<td><b>'+ k.toUpperCase() +'</b></td><td>';
+        //                          for (var l in temp2) {
+        //                          if (l.match("primary"))
+        //                           continue;
+        //                           userdetail += temp[k][l] +' ';
+        //                           }
+        //                           userdetail += '</td></tr>';
+        //                       }
+        //                     }
+        //                  else {
+        //                   continue;
+        //                 }
+        //               }}} else {
+        //                 continue;
+        //               }
+        //             }
+        //             userdetail += '</td></tr>';
+        //             //userdetail += "Name  :"+result[key].firstName + result[key].lastName +"<br>"+ "gender:" +result[key].gender+"<br>";
+        //           }
+        //           userdetail += '</table>';
+        //           document.getElementById("SearchResult").innerHTML = userdetail;
+        //         }
+        //
+        // });
--- server/create/SearchIdServer.js
+++ server/create/SearchIdServer.js
@@ -1,17 +1,18 @@
 Meteor.methods({
    "SearchUser": function (name) {
-
      //Check is made for name
      check(name, String);
      console.log("The name is :" + name);
-     count_value =Meteor.users.find({ 'profile.firstName': name}).count();
+    // Meteor.users.createindex({'profile.firstName':1});
+      //Meteor.users.createindex({'profile.lastName':1});
+     count_value =Meteor.users.find({$or:[{'profile.firstName':{"$regex": "^" + name + "\\b", "$options": "i"}},{'profile.lastName': {"$regex": "^" + name + "\\b", "$options": "i"}}]}).count();
      console.log(count_value);
-     if (count_value == 0) {
-       return 0;
-     } else {
+    //  if (count_value == 0) {
+    //    return 0;
+    //  } else {
        data = []
-       data =Meteor.users.find({'profile.firstName':name}).fetch();
+       data =Meteor.users.find({$or:[{'profile.firstName':{"$regex": "^" + name + "\\b", "$options": "i"}},{'profile.lastName': {"$regex": "^" + name + "\\b", "$options": "i"}}]}).fetch();
        return data;
-     }
+             // }
  }
 });
