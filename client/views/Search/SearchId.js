919cf09e15cacacffe8cfc092821a5c005b53f6d
Tamilppavai Ramamoorthy <tramamoorthy@paypal.com>
2015-10-18 21:01:47 +0530

    search- request and profile

--- client/views/Search/SearchId.js
+++ client/views/Search/SearchId.js
@@ -1,74 +1,110 @@
 //Session.setDefault("res","Self");
-Template.SearchId.helpers({
-      'Search': function () {
+Template.SearchId.helpers ({
+      'isUser': function () {
         check();
       return (Session.get('res'));
+    },
+    'searchuser': function () {
+         //console.log("Search" + JSON.stringify('Session.get('searchResult')') );
+         return (Session.get('searchResult'));
+       },
+    'IsShare':  function(){
+      return (Session.get('isShare'));
+    },
+    'getUserRequest':function() {
+      return(Session.get('getUserRequest'));
+    },
+    'getUserProfile' :function() {
+      return(Session.get('userProfileDetails'));
     }
+
 });
 
-//UserprofileDummy = new Meteor.Collection('userprofiles');
-
-
 Template.SearchId.events({
-    //'click .search': function (event, next) {
-      //  name = document.getElementById('name').value;
-             'submit.search': function (event) {
+  'click .Request': function(event){
+    event.preventDefault();
+    console.log("clicked request");
+  var UserReq = Session.get('searchResult');
+  for(var key in UserReq)
+ {
+   console.log (key);
+  for(var field in UserReq[key]){
+    if(field.match('_id'))
+    var userId= UserReq[key]._id;
+  }}
+ console.log(userId);
+ Meteor.call("SearchRequest",userId, function(error, result) {
+   if (result == 0)
+   {
+     alert("No Request found for this User");
+   }
+   else {
+     Session.set('getUserRequest',result);
+     console.log("Requests:"+JSON.stringify(result));
+       }
+
+
+ });
+   }
+   ,
+   'click .Profile': function(event){
+     event.preventDefault();
+ console.log("Clicked Profile");
+ var userDetails=Session.get('searchResult');
+ Session.set('userProfileDetails',userDetails);
+ console.log("userprofile:"+JSON.stringify('Session.get(userProfileDetails)'));
+   }
+   ,
+       'submit form': function (event) {
         event.preventDefault();
         EnteredName = event.target.Ename.value;
         console.log('clicked Search ' + EnteredName );
         Meteor.call("SearchUser",EnteredName, function(error, result) {
-               //console.log ("the result is:" User_result[0].name)
-              // console.log("Client : error" + error + JSON.stringify(result));
-               if (result == 0) {
+                           if (result == 0) {
                  console.log("No Result Found.");
                  alert("No Result Found !")
                }
                else {
-                 Session.set('res',result);
+                 Session.set('searchResult',result);
+                console.log("Search" + JSON.stringify(Session.get('searchResult')));
+              }
 
-                 var userview = '<table border=2 >';
-                 for(var key in result)
-                 {
-                   userview += '<tr>';
-                   for(var field in result[key]){
-                     if (typeof result[key][field] == 'object') {
-                       if (field.match("profile")) {
-                         var temp = {};
-                         temp = result[key].profile;
-                         for (var k in temp) {
-                           console.log("My test: " + k);
-                           if (k.match("firstName") || k.match("lastName") )
-                           {
-                                  userview += '<td>'+'<b>'+ k.toUpperCase() +'</b>'+ '</td>' ;
-                                 userview += '<td>'+temp[k] +'</br>'+'</td>';
-                                 userview += '</td></tr>';
-                               //(k.match("sharePhone") and temp[k]='yes')
-                             }
-                                                    }
-                        }
-                      }
-                    }
-                  }
-                    userview += '<td>'+ '<button type="button" class="view" value= "view" >'+'view'+'</button>'+' </td>';
-                  //userview += '<td>' + '<a href="Router.go('/');">'+'view'+ '</a></td>';
-                userview += '</table>';
-           }
-                 document.getElementById("SearchResult").innerHTML = userview;
-
-              });
-            },
-
-            'click.view': function () {
-       event.preventDefault();
-       userprofileDetail={}
-        userprofileDetail=Session.get('res');
-           console.log('clicked view '+ + JSON.stringify(userprofileDetail));
-      // Meteor.call("SearchUser",EnteredName, function(error, result) {
-
-            console.log("value is:"+Session.get('res'));
+             });
           }
 
        });
+
+
+// Template.SearchRequest.onRendered(function(){
+//
+//
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- server/create/SearchIdServer.js
+++ server/create/SearchIdServer.js
@@ -1,18 +1,24 @@
 Meteor.methods({
+
    "SearchUser": function (name) {
      //Check is made for name
      check(name, String);
      console.log("The name is :" + name);
-    // Meteor.users.createindex({'profile.firstName':1});
-      //Meteor.users.createindex({'profile.lastName':1});
      count_value =Meteor.users.find({$or:[{'profile.firstName':{"$regex": "^" + name + "\\b", "$options": "i"}},{'profile.lastName': {"$regex": "^" + name + "\\b", "$options": "i"}}]}).count();
      console.log(count_value);
-    //  if (count_value == 0) {
-    //    return 0;
-    //  } else {
        data = []
        data =Meteor.users.find({$or:[{'profile.firstName':{"$regex": "^" + name + "\\b", "$options": "i"}},{'profile.lastName': {"$regex": "^" + name + "\\b", "$options": "i"}}]}).fetch();
+       details = data;
        return data;
-             // }
+
+ },
+ "SearchRequest": function(id) {
+     check(id,String);
+     console.log("request server",id);
+   count_value =Request.find({requestorId:id}).count();
+   console.log(count_value);
+   requestData=[]
+   requestData=Request.find({requestorId:id}).fetch();
+   return requestData;
  }
 });
