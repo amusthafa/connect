56464ba26eb6508705057050a2ac23adcc1498da
sfathima <sfathima@paypal.com>
2015-10-22 14:24:00 +0530

    Notifications commit

--- client/views/notifications/notificationsClient.js
+++ client/views/notifications/notificationsClient.js
@@ -1,50 +1,49 @@
-Template.notifications.helpers({notifications: function() {
-    check();
-    var req = Session.get('notifications');
-    return (Session.get('notifications'));
-}
+Template.notifications.helpers({
+    notifications: function () {
+        check();
+        var req = Session.get('notifications');
+        return (Session.get('notifications'));
+    }
 
 });
 
-Template.notifications.onRendered(function() {
-    var user = { userId :Meteor.userId() };
-//    alert('result - ');
+Template.notifications.onRendered(function () {
+    var user = {userId: Meteor.userId()};
 
-        Meteor.call('getNotifications', user, function(err, result) {
-
-  //          alert('result - '+JSON.stringify(result));
-            console.log("on rendered result:", JSON.stringify(result));
-            Session.set('notifications', result);
-        });
+    Meteor.call('getNotifications', user, function (err, result) {
+        console.log("on rendered result:", JSON.stringify(result));
+        Session.set('notifications', result);
+    });
 
 });
-
 
 
 Template.notifications.events({
-    'click #getRequest': function(event) {
+    'click #getRequest': function (event) {
         event.preventDefault();
-        var connect = {_id : event.target.getAttribute("data-id"),
-         notificationId : event.target.getAttribute("data-notif-id")};
+        var connect = {
+            _id: event.target.getAttribute("data-id"),
+            notificationId: event.target.getAttribute("data-notif-id")
+        };
 
-    /*    Meteor.call('getRequest', request, function(err, result) {
-            console.log(" result:", JSON.stringify(result));
-            Session.set('req', result);
-           alert(result);
-           Router.go("/manageRequest");
-        });
+        /*    Meteor.call('getRequest', request, function(err, result) {
+         console.log(" result:", JSON.stringify(result));
+         Session.set('req', result);
+         alert(result);
+         Router.go("/manageRequest");
+         });
 
-*/
+         */
         //call connect, update request status,
         // get connect details for the volunteer to update the connect status to accept/decline
 
-        Meteor.call('getConnectDetails', connect, function(err, result) {
-                console.log(" result:", JSON.stringify(result));
-                Session.set('connectDetails', result);
-         //       alert(result);
-                Router.go("/connectUpdate");
+        Meteor.call('getConnectDetails', connect, function (err, result) {
+            console.log(" result:", JSON.stringify(result));
+            Session.set('connectDetails', result);
+            //       alert(result);
+            Router.go("/connectUpdate");
 
-            });
+        });
     },
     'submit form': function (event) {
         event.preventDefault();
--- server/create/notificationsServer.js
+++ server/create/notificationsServer.js
@@ -1,47 +1,55 @@
 Meteor.methods(
     {
         getNotifications: function (user) {
-            check(user,Object);
-            check(user.userId ,String);
-            console.log('getNotifications entered' + JSON.stringify( user));
+            check(user, Object);
+            check(user.userId, String);
+            console.log('getNotifications entered' + JSON.stringify(user));
             //userId : user.userId
             var notificationsList = Notifications.find(
                 {
-                 $query: {
-                         "userId" : user.userId, "status" : 'Unread'
-                     }
-                ,
-                $orderby: { rowCreated : -1 }
-            }
-).fetch();
-            /*
-            var notificationsArr = [];
+                    $query: {
+                        "userId": user.userId, "status": 'Unread'
+                    }
+                    ,
+                    $orderby: {rowCreated: -1}
+                }
+            ).fetch();
+
             for (var i in notificationsList) {
+                var notification = notificationsList[i];
+                var id = notification.description.split("for")[1].replace(" ", "");
+                var request = Aid.findOne({_id: id});
+                console.log(notification.description.split("for")[0] + "for " + request.aidName);
+                notification.description = notification.description.split("for")[0] + "for " + request.aidName;
 
                 var notification = notificationsList[i];
-                //   console.log("volunteer - "+ volunteer);
-                notificationsArr.push(notification._id);
-
+                var request = Request.findOne({_id: notification.requestId});
+                console.log(request.request_Name);
             }
-            console.log(notificationsArr);
-
-            //update the status as read for retrieved notifications
 
 
-            Notifications.update( {_id: { $in: notificationsArr }}, { $set :{"status": "Read"}}
-                , function (error, result) {
-                    console.log("result " + result + ' error ' + error );
-                    if (error) {
-                        console.log("Errors !!" + error + "  Result - " + result);
-                        //TO-DO: error message()
-                        // throw new Meteor.Error("insert-failed", error.message);
-                        throw new Meteor.Error("update-failed", error);
-                    }
-                });
-*/
-
-            console.log('notificationsList  ' + notificationsList);
+            console.log('notificationsList  ' + JSON.stringify(notificationsList));
             return notificationsList;
+
+
+
+
+            /*
+
+             //update the status as read for retrieved notifications
+
+
+             Notifications.update( {_id: { $in: notificationsArr }}, { $set :{"status": "Read"}}
+             , function (error, result) {
+             console.log("result " + result + ' error ' + error );
+             if (error) {
+             console.log("Errors !!" + error + "  Result - " + result);
+             //TO-DO: error message()
+             // throw new Meteor.Error("insert-failed", error.message);
+             throw new Meteor.Error("update-failed", error);
+             }
+             });
+             */
         }
     }
 );
