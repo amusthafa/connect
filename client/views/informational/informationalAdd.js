71b1cd1879aae35b67799f1dc784c44adf334b49
Tamilppavai Ramamoorthy <tramamoorthy@paypal.com>
2015-10-23 15:13:15 +0530

    info model

--- client/views/informational/informationalAdd.js
+++ client/views/informational/informationalAdd.js
@@ -1,39 +1,39 @@
-Template.informationAdd.helpers ({
-  'info':function() {
-    return(Session.get('info'));
-      },
-'hosp':function() {
-  return(Session.get('hosp'));
-}
-});
-
-Template.informationAdd.events({
-
-  'submit form': function (event) {
-    event.preventDefault();
-    console.log('adding');
-   var detid=  $('input[name=hosp]:radio:checked').val()
-   console.log(detid);
-    Session.set('info',detid);
-    var detail_type = Session.get('info');
-    switch(detail_type){
-      case 'hosp':
-      {
-            Session.set('hosp',1);
-          var addhosp={}
-          addhosp.Hospital_name=event.target.Hospital_name.value;
-        addhosp.Doctor_name=event.target.Doctor_name.value;
-        addhosp.Facility_desc=event.target.Facility_desc.value;
-        console.log("hosp:"+JSON.stringify(addhosp));
-        break;
-      }
-        default :
-        {
-console.log("default");
-        }
-    }
-
-
-
-  }
-});
+// Template.informationAdd.helpers ({
+//   'info':function() {
+//     return(Session.get('info'));
+//       },
+// 'hosp':function() {
+//   return(Session.get('hosp'));
+// }
+// });
+//
+// Template.informationAdd.events({
+//
+//   'submit form': function (event) {
+//     event.preventDefault();
+//     console.log('adding');
+//    var detid=  $('input[name=hosp]:radio:checked').val()
+//    console.log(detid);
+//     Session.set('info',detid);
+//     var detail_type = Session.get('info');
+//     switch(detail_type){
+//       case 'hosp':
+//       {
+//             Session.set('hosp',1);
+//           var addhosp={}
+//           addhosp.Hospital_name=event.target.Hospital_name.value;
+//         addhosp.Doctor_name=event.target.Doctor_name.value;
+//         addhosp.Facility_desc=event.target.Facility_desc.value;
+//         console.log("hosp:"+JSON.stringify(addhosp));
+//         break;
+//       }
+//         default :
+//         {
+// console.log("default");
+//         }
+//     }
+//
+//
+//
+//   }
+// });
--- models/informationalModel.js
+++ models/informationalModel.js
@@ -1,8 +1,5 @@
-Medical = new Meteor.Collection('medicalInfo');
-Education = new Meteor.Collection('educationInfo');
-Ailments = new Meteor.Collection('ailmentsInfo');
-Jobs = new Meteor.Collection('jobInfo');
-Rights = new Meteor.Collection('rightsInfo');
+Information = new Meteor.Collection('information');
+
 
 Address = new SimpleSchema({
     line1: {
@@ -24,116 +21,55 @@ Address = new SimpleSchema({
         type: String
     }
 });
-Medical.attachSchema({
-  Hospital_name: {
-    type: String
-  },
-  Doctor_name: {
-    type: String
-  },
-  Facility_desc: {
-    type: String
-  }
-});
 
-Education.attachSchema({
-Institution_name: {
-    type: String
-  },
-  ContactPerson_name: {
-    type: String
-  },
-  Institute_Address: {
-    type: Address
-  },
-  Facility_desc: {
-    type: String
-  }
-  });
 
-Ailments.attachSchema({
-  Ailments_name: {
-  type: String
-  },
-  Ailments_type: {
-    type: String
-  },
-  Ailments_desc:{
-    type: String
-  },
-  hosp_info :{
-    type : String
-  }
-  });
+Information.attachSchema({
+        info_Title: {
+            type: String
+        },
+        infoType: {
+            type: String,
+            allowedValues: ['Hospitals/Doctors','Ailments','Education','Jobs','Rights']
+        },
+        informationDescription: {
+          type:String
 
-  Jobs.attachSchema({
-    Company_name: {
-        type: String
-    },
-    Job_resignation: {
-      type: String
-    },
+        },
+       contactAddress: {
+            type: Address
+            optional:true
+        },
 
-    EligibilityInfo:{
-      type: String
-    },
+        contactPPhone:{
+            type:Number
+          optional:true
+        },
+        addedBy_Id: {
+              type: String
+          },
 
-    QualificationRequired: {
-      type: String
-    },
-
-    Company_Address: {
-        type: Address
-      },
-
-    Job_desc: {
-        type: String
-      }
-  });
-
-  Rights.attachSchema({
-    Rights_name: {
-      type: String
-    },
-
-    Rights_secno:{
-      type: String
-    },
-    ApplicableTo:{
-      type: String
-    },
-    Rights_details:{
-      type: String
+        rowCreated: {
+            type: Date,
+            denyUpdate: true,
+            autoValue: function () {
+                if (this.isInsert) {
+                    return new Date;
+                } else if (this.isUpsert) {
+                    return {$setOnInsert: new Date};
+                } else {
+                    this.unset();  // Prevent user from supplying their own value
+                }
+            }
+        },
+        rowUpdated: {
+            type: Date,
+            autoValue: function () {
+                if (this.isUpdate) {
+                    return new Date();
+                }
+            },
+            denyInsert: true,
+            optional: true
+        }
     }
-  });
-
-
-
-
-
-
-//           rowCreated: {
-//             type: Date,
-//             denyUpdate: true,
-//             autoValue: function () {
-//                 if (this.isInsert) {
-//                     return new Date;
-//                 } else if (this.isUpsert) {
-//                     return {$setOnInsert: new Date};
-//                 } else {
-//                     this.unset();  // Prevent user from supplying their own value
-//                 }
-//             }
-//         },
-//         rowUpdated: {
-//             type: Date,
-//             autoValue: function () {
-//                 if (this.isUpdate) {
-//                     return new Date();
-//                 }
-//             },
-//             denyInsert: true,
-//             optional: true
-//         }
-//     }
-// );
+);
