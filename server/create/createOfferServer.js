2005926b4614ced0ebde7090d00cd5dc13d804ae
pbijja <pbijja@paypal.com>
2015-10-25 17:50:01 +0530

    Offer Error and success handling

--- server/create/createOfferServer.js
+++ server/create/createOfferServer.js
@@ -13,85 +13,72 @@ Meteor.methods(
             }
             return request;
         },
-        createOffer: function (OferId) {
-        check(OferId, Object);
-        if (OferId.volunteerId === Meteor.userId()) {
-            volunteerId = OferId.volunteerId;
-        } else if (OferId.volunteerId !== Meteor.userId()) {
-            var emailId = OferId.requestorId.split("-")[1];
-            id = JSON.stringify(Accounts.findUserByEmail(emailId));
-            volunteerId = id.split(":")[1].split(",")[0].replace("\"", "").replace("\"", "");
-        }
+        createOffer: function (offer) {
+            check(offer, Object);
+            if (offer.volunteerId === Meteor.userId()) {
+                volunteerId = offer.volunteerId;
+            } else if (offer.volunteerId !== Meteor.userId()) {
+                var emailId = offer.requestorId.split("-")[1];
+                id = JSON.stringify(Accounts.findUserByEmail(emailId));
+                volunteerId = id.split(":")[1].split(",")[0].replace("\"", "").replace("\"", "");
+            }
+            ;
 
-        var offer = {
-            "offerName": OferId.offerName,
-            "offerType": OferId.offerType,
-            "creatorId": OferId.creatorId,
-            "volunteerId": volunteerId,
-            "offerAddress": {
-                "line1": OferId.line1,
-                "line2": OferId.line2,
-                "city": OferId.city,
-                "state": OferId.state,
-                "country": OferId.country,
-                "pinCode": OferId.pincode,
-            },
-            "comment": OferId.comment,
-            "row_created": new Date(),
-            "row_updated": new Date()
-        };
-
-        var offerId;
-
-        console.log("offer!!!:", JSON.stringify(offer));
-
-        Offer.insert(offer, function (error, result) {
-
-          console.log("offer insert " + JSON.stringify(result));
-          if (error) {
+            var offerObj = {
+                "offerName": offer.offerName,
+                "offerType": offer.offerType,
+                "creatorId": offer.creatorId,
+                "volunteerId": volunteerId,
+                "offerAddress": {
+                    "line1": offer.line1,
+                    "line2": offer.line2,
+                    "city": offer.city,
+                    "state": offer.state,
+                    "country": offer.country,
+                    "pinCode": offer.pincode,
+                },
+                "comment": offer.comment,
+                "row_created": new Date(),
+                "row_updated": new Date()
+            };
+            Offer.insert(offerObj, function (error, result) {
+              console.log("offer insert result:" + JSON.stringify(result));
+                if (error) {
               console.log("offer sanitizedError!!!:", error.sanitizedError);
               throw new Meteor.Error(error.sanitizedError.error, error.message, error.sanitizedError.details);
           }
           else{
-            try{
-            console.log("offer insert success: result:", JSON.stringify(result));
-            offerId = result;
-            console.log("offerID!!!!!!!!!!!!:", offerId);
-            var volunteerAid = {
-              "offerId": offerId,
-              "volunteerId": volunteerId,
-              "aidId": OferId.aid,
-              "aidStart": OferId.fromDate,
-              "aidExpiry": OferId.toDate,
-              "aidAddress": {
-                  "line1": OferId.line1,
-                  "line2": OferId.line2,
-                  "city": OferId.city,
-                  "state": OferId.state,
-                  "country": OferId.country,
-                  "pinCode": OferId.pincode,
-                },
-              "row_created": new Date(),
-              "row_updated": new Date()
-              };
+              console.log("Success insert in Offer table");
+                var volunteerAid = {
+                        "offerId": result,
+                        "volunteerId": volunteerId,
+                        "aidId": offer.aid,
+                        "aidStart": offer.fromDate,
+                        "aidExpiry": offer.toDate,
+                        "aidAddress": {
+                            "line1": offer.line1,
+                            "line2": offer.line2,
+                            "city": offer.city,
+                            "state": offer.state,
+                            "country": offer.country,
+                            "pinCode": offer.pincode,
+                        },
+                        "row_created": new Date(),
+                        "row_updated": new Date()
+                    };
+                VolunteerAid.insert(volunteerAid, function (error, result) {
+                  console.log("volunteerAid insert result:" + JSON.stringify(result));
 
-              console.log("VolunteerAid:", JSON.stringify(volunteerAid));
-
-            VolunteerAid.insert(volunteerAid, function (error, result) {
-                console.log("volunteerAid insert " + JSON.stringify(result));
-                if (error) {
+                  if (error) {
                     console.log(" volunteerAid sanitizedError!!!:", error.sanitizedError);
                     throw new Meteor.Error(error.sanitizedError.error, error.message, error.sanitizedError.details);
+                }else{
+                  console.log("Success insert in VolunteerAid table");
                 }
-            });
-          }
-          catch (e) {
-              console.log("error caught!!:", e);
-              // throw new Meteor.Error(e,"Please fill in the required details");
-              throw new Meteor.Error(e, e.reason, e.details);
-          }
-          }
-        });
 
-      }
+                    });
+              }
+            });
+
+        }
     });
