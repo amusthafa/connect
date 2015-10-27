cea87a55bf6e1554718c39e8ae684bd231274bfb
amusthafa <amusthafa@paypal.com>
2015-10-27 17:00:15 +0530

    Manual connect tempalte changes

--- client/views/manualConnect/manualConnectClient.js
+++ client/views/manualConnect/manualConnectClient.js
@@ -1,133 +1,41 @@
-//Session.setDefault("res","Self");
-Template.SearchId.onRendered(function() {
-  $('.rateit').rateit();
-  $(".rateit").on('rated', function (event, value) {
-            console.log('Rating:' + value)
-            })
-});
 
-Template.SearchId.helpers ({
-      'isUser': function () {
-        check();
-      return (Session.get('res'));
-    },
-    'isAdmin': function () {
-      return (Session.get('isAdmin'));
-    },
-    'searchuser': function () {
-         //console.log("Search" + JSON.stringify('Session.get('searchResult')') );
-         return (Session.get('searchResult'));
-       },
-    'IsShare':  function(){
-      return (Session.get('isShare'));
-    },
-    'getUserRequest':function() {
-      return(Session.get('getUserRequest'));
-    },
-    'getUserProfile' :function() {
-      return(Session.get('getUserProfile'));
-    },
-    'getSelectedUserId' :function() {
-    return(Session.get('userId'));
+
+Template.manualConnect.helpers ({
+
+    'SearchUserforManual': function () {
+        //console.log("Search" + JSON.stringify('Session.get('searchResult')') );
+        return (Session.get('SearchUserforManual'));
     }
-  });
-
-
-
-Template.SearchId.events({
-  'click .Request': function(event){
-    event.preventDefault();
-    console.log("clicked request");
-    var UserReq = Session.get('searchResult');
-  if($('input[name=SelectUser]:radio:checked').val())
- {
-for(var key in UserReq)
-{
- id_user=UserReq[key]._id;
-if($('input[name=SelectUser]:radio:checked').val() === id_user)
-{
-Session.set('userId',UserReq[key]._id);
-userId= UserReq[key]._id;
-}
- else continue;
- console.log(userId);
- Meteor.call("SearchRequest",userId, function(error, result) {
-   if (result == 0)
-   {
-     sAlert.error("No Request found for this User");
-   }
-   else {
-     Session.set('getUserRequest',result);
-     Session.set('getUserProfile',0);
-     console.log("Requests:"+JSON.stringify(result));
-       }
 });
-   }}
-   else {
-     sAlert.error("Select a User");
-   }}
-   ,
-   'click .Profile': function(event){
-     event.preventDefault();
- console.log("Clicked Profile");
- var UserReq = Session.get('searchResult');
- if($('input[name=SelectUser]:radio:checked').val())
- {
-for(var key in UserReq)
-{
-id_user=UserReq[key]._id;
-if($('input[name=SelectUser]:radio:checked').val() === id_user)
-{
-Session.set('userId',UserReq[key]._id);
-userId= UserReq[key]._id;
-}
-else continue;
-if (Roles.userIsInRole(userId,'Admin'))
-{ console.log("is admin : " + userId);
-  Session.set('isAdmin','true');
-console.log(Session.get('isAdmin'));}
-Meteor.call("SearchProfile",userId, function(error, result) {
-    Session.set('getUserProfile',result);
-    Session.set('getUserRequest',0);
-    });
- }}
 
-else {
-  sAlert.error("Select a User");
-}
-}
- ,
-       'submit form': function (event) {
+Template.manualConnect.events({
+
+    'click .searchUserforManual' : function (event) {
         event.preventDefault();
+
+
         Session.set('getUserProfile',0);
         Session.set('getUserRequest',0);
         Session.set('searchResult',0);
-        EnteredName = event.target.Ename.value;
+        EnteredName = $('#Ename').val();
+
         if(EnteredName) {
-        Meteor.call("SearchUser",EnteredName, function(error, result) {
-                           if (result == 0) {
-                 sAlert.error("No Result Found !")
-               }
-               else
-                   Session.set('searchResult',result);
-               });
-         }
-         else
-         sAlert.error("Please enter a name to search");
+            Meteor.call("SearchUser",EnteredName, function(error, result) {
+                if (result == 0) {
+                    sAlert.error("No Result Found !")
+                }
+                else
+                    Session.set('SearchUserforManual',result);
+                alert('result- '+JSON.stringify(result));
+            });
+        }
+        else
+            sAlert.error("Please enter a name to search");
 
-          },
-          'click .addAdmin': function(event){
-              console.log("inside add admin");
-              Roles.addUsersToRoles( Session.get('userId'), ['Admin']);
-              // Meteor.users.update({_id : Session.get('userId')}, {$set : {roles : "Admin"}});
-          }
-       });
+    }
 
 
-// Template.SearchRequest.onRendered(function(){
-//
-//
-// });
+});
 
 
 
