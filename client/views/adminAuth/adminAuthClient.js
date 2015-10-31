476e68146863d7573a0eb5bd31cf4a7478080951
sfathima <sfathima@paypal.com>
2015-10-31 17:57:16 +0530

    removing alert

--- client/views/adminAuth/adminAuthClient.js
+++ client/views/adminAuth/adminAuthClient.js
@@ -1,5 +1,4 @@
-
-Template.adminAuth.helpers ({
+Template.adminAuth.helpers({
 
     'searchUser': function () {
         console.log("Search" + JSON.stringify(Session.get('searchResult')));
@@ -10,70 +9,70 @@ Template.adminAuth.helpers ({
         console.log("getUserName" + JSON.stringify(Session.get('selectedUser')));
         return (Session.get('selectedUser'));
     }
-  });
+});
 
-  Template.adminAuth.onDestroyed(function () {
+Template.adminAuth.onDestroyed(function () {
 
-      delete Session.keys['selectedUser'];
-      delete Session.keys['searchUser'];
-  });
+    delete Session.keys['selectedUser'];
+    delete Session.keys['searchUser'];
+});
 
 Template.adminAuth.events({
 
-      'click .searchUser' : function (event) {
-          event.preventDefault();
-          console.log("clicked on searchUser!!");
+    'click .searchUser': function (event) {
+        event.preventDefault();
+        console.log("clicked on searchUser!!");
 
-          // Session.set('getUserProfile',0);
-          // Session.set('getUserRequest',0);
-          Session.set('searchResult',0);
-          EnteredName = $('#Ename').val();
+        // Session.set('getUserProfile',0);
+        // Session.set('getUserRequest',0);
+        Session.set('searchResult', 0);
+        EnteredName = $('#Ename').val();
 
-          if(EnteredName) {
-              Meteor.call("SearchUser",EnteredName, function(error, result) {
-                  if (result == 0) {
-                      sAlert.error("No Result Found !")
-                  }
-                  else
-                      Session.set('searchUser',result);
-                  alert('result- '+JSON.stringify(result));
-              });
-          }
-          else
-              sAlert.error("Please enter a name to search");
+        if (EnteredName) {
+            Meteor.call("SearchUser", EnteredName, function (error, result) {
+                if (result == 0) {
+                    // sAlert.error("No Result Found !")
+                }
+                else
+                    Session.set('searchUser', result);
+                // alert('result- '+JSON.stringify(result));
+            });
+        }
+        else
+        //  sAlert.error("Please enter a name to search");
+            console.log("")
+    },
 
-      },
+    'click .selectUser': function (event) {
+        event.preventDefault();
+        console.log("Selcected this user");
 
-      'click .selectUser': function(event){
-          event.preventDefault();
-          console.log("Selcected this user");
-
-                  // Meteor.call("SearchProfile", this._id, function(error, result) {
-                  //     Session.set('getUserProfileforManual',result);
-                  //     Session.set('getUserRequestforManual',0);
-                  //     alert('result' +result);
-                  // });
-          Session.set("selectedUser",this._id);
-      },
+        // Meteor.call("SearchProfile", this._id, function(error, result) {
+        //     Session.set('getUserProfileforManual',result);
+        //     Session.set('getUserRequestforManual',0);
+        //     alert('result' +result);
+        // });
+        Session.set("selectedUser", this._id);
+    },
 
     'submit form': function (event) {
         event.preventDefault();
         //console.log('form submitted');
         var adminAuth = {};
-             adminAuth.user = event.target.user.value;
+        adminAuth.user = event.target.user.value;
         adminAuth.status = event.target.status.value;
 
         Meteor.call("updateAdminAuth", adminAuth, function (error, result) {
             console.log("Client : error" + error + "result - " + result);
             if (error) {
-              console.log("error body", (error));
-              sAlert.error(error.reason);
-              Router.go("/loadAdminAuth");
+                console.log("error body", (error));
+                // sAlert.error(error.reason);
+                Router.go("/loadAdminAuth");
             }
-            else{
-              console.log("success");
-                var succMsg="Successfully updated!";
-              }
+            else {
+                console.log("success");
+                var succMsg = "Successfully updated!";
+            }
         });
         Router.go("/");
     }
