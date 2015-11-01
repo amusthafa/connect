9eb4749fe66c8573b8cbfea14435cb554046caee
VardhiniM <vardhini.m25@gmail.com>
2015-11-01 12:31:19 +0530

    removed duplicate in carousel

--- server/service/homeServer.js
+++ server/service/homeServer.js
@@ -1,22 +1,22 @@
 Meteor.methods({
   "getDocInfo" : function(){
-    DocInfo=Information.find({infoType : "Hospitals/Doctors"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
+    DocInfo=Information.find({infoType : "Hospitals/Doctors", verificationStatus : "Yes"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
     return DocInfo;
   },
   "getAilInfo" : function(){
-    AilInfo=Information.find({infoType : "Ailments"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
+    AilInfo=Information.find({infoType : "Ailments", verificationStatus : "Yes"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
     return AilInfo;
   },
   "getEduInfo" : function(){
-    EduInfo=Information.find({infoType : "Education"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
+    EduInfo=Information.find({infoType : "Education", verificationStatus : "Yes"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
     return EduInfo;
   },
   "getJobInfo" : function(){
-    JobInfo=Information.find({infoType : "Jobs"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
+    JobInfo=Information.find({infoType : "Jobs", verificationStatus : "Yes"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
     return JobInfo;
   },
   "getRightsInfo" : function(){
-    RightsInfo=Information.find({infoType : "Rights"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
+    RightsInfo=Information.find({infoType : "Rights", verificationStatus : "Yes"}, {limit : 3, sort: {rowCreated: -1}}).fetch();
     return RightsInfo;
   }
 });
