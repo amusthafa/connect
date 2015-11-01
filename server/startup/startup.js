76987401d05cb45fb461e3dd8f5224747fcf8841
Tamilppavai Ramamoorthy <tramamoorthy@paypal.com>
2015-11-01 18:44:06 +0530

    Accounts email content and subject

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -4,20 +4,49 @@
 Meteor.startup(function () {
     SyncedCron.start();
     process.env.MAIL_URL='smtp://olaamigo.app%40gmail.com:123456xyz@smtp.gmail.com:465/';
-    Accounts.emailTemplates.from = 'Ola Amigos ';
 
-     //-- Application name
-     Accounts.emailTemplates.siteName = 'ola_Amigos'
-    //-- Subject line of the email.
+    Accounts.emailTemplates = {
+  from: "Ola Amigos  <olaamigo.app@gmail.com>",
+  siteName: Meteor.absoluteUrl().replace(/^https?:\/\//, '').replace(/\/$/, ''),
 
-  Accounts.emailTemplates.verifyEmail.subject = function(user) {
-    return 'Confirm Your Email Address for Ola Amigos';
-  };
+  resetPassword: {
+    subject: function(user) {
+      return "To reset your password for Ola Amigos " ;
+    },
+    text: function(user, url) {
+      var greeting = (user.profile && user.profile.firstName) ?
+            ("Hello " + user.profile.firstName + ",") : "Hello,";
+return greeting + "\n"
+              + "\n"
+              + "To Reset password for your account, simply click the link below.\n"
+              + "\n"
+              + url + "\n"
+              + "\n"
+              + "Thanks,\n"
+              +"Amigos";
+            }
+          },
+        verifyEmail: {
+      subject: function(user) {
+        return "Confirm Your Email Address for Ola Amigos ";
+      },
+       text : function(user, url) {
+         var greeting = (user.profile && user.profile.firstName) ?
+               ("Hello " + user.profile.firstName + ",") : "Hello,";
 
-  //-- Email text
-  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
-    return 'Thank you for registering.  Please click on the following link to verify your email address: \r\n' + url;
-  };
+       return  greeting + "\n"
+                     + "\n"
+                     +'Thank you for registering with us.\n'
+                     +"\n"
+                     +'Please click on the following link to verify your email address: \r\n'
+                     +"\n"
+                     + url+ "\n"
+                     +"\n"
+                     + "Thanks,\n"
+                     +"Amigos";
 
+    }
+  }
+};
 
-});
+ });
