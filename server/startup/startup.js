29350c8fe4577837d7f144385ea96acd18c03a27
Tamilppavai Ramamoorthy <tramamoorthy@paypal.com>
2015-11-01 15:59:27 +0530

    email

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -4,5 +4,15 @@
 Meteor.startup(function () {
     SyncedCron.start();
     process.env.MAIL_URL='smtp://olaamigo.app%40gmail.com:123456xyz@smtp.gmail.com:465/';
+    //-- Subject line of the email.
+  Accounts.emailTemplates.verifyEmail.subject = function(user) {
+    return 'Confirm Your Email Address for Ola Amigos';
+  };
+
+  //-- Email text
+  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+    return 'Thank you for registering.  Please click on the following link to verify your email address: \r\n' + url;
+  };
+
 
 });
